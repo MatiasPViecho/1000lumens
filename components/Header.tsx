c3e4d04f83e56fdaa4ab9d69688f415a3321d5db
Matias V <mpviecho@gmail.com>
2024-03-13 13:45:54 -0300

    feat: add header

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+    return(
+        <header className="sticky top-0 pt-2 pb-3 bg-white/30 backdrop-blur-sm max-w-72 mx-auto rounded-b-full">
+            <ul className="flex gap-4 justify-center">
+                <li>Inicio</li>
+                <li>Mis Fotos</li>
+                <li>Sobre Mí</li>
+            </ul>
+        </header>
+        
+    )
+}
