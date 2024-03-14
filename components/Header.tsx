9c5a744957b54786761ed96dd801286003711ad1
Matias V <mpviecho@gmail.com>
2024-03-14 00:45:26 -0300

    feat: style improvements in Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,10 @@
 export const Header = () => {
     return(
         <header className="fixed top-0 z-50 w-full ">
-            <ul className="flex gap-4 justify-center pt-2 pb-3 shadow-lg bg-black/50 backdrop-blur-sm max-w-72  rounded-b-full  text-white mx-auto">
-                <li>Inicio</li>
-                <li>Mis Fotos</li>
-                <li>Sobre Mí</li>
+            <ul className="flex gap-4 justify-center pt-2 pb-3 shadow-lg backdrop-saturate-200 backdrop-brightness-[0.3] backdrop-grayscale backdrop-invert backdrop-blur-sm max-w-80 rounded-bl-3xl overflow-hidden text-white ml-auto">
+                <li className="text-md tracking-widest text-black font-extralight uppercase">Inicio</li>
+                <li className="text-md tracking-widest text-black font-extralight uppercase">Mis Fotos</li>
+                <li className="text-md tracking-widest text-black font-extralight uppercase">Sobre Mí</li>
             </ul>
         </header>
         
