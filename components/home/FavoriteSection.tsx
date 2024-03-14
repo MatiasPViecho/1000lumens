8c54832e87872560dc2a972f9d2ab8ed59b28f08
Matias V <mpviecho@gmail.com>
2024-03-14 04:25:16 -0300

    chore: improve image for middle section in desktop

--- components/home/FavoriteSection.tsx
+++ components/home/FavoriteSection.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export const FavoriteSection = () => {
   return (
     <>
@@ -10,25 +12,37 @@ export const FavoriteSection = () => {
         </h6>
       </div>
       <ul className="flex w-full gap-8 overflow-x-scroll px-4 py-2 pb-4 snap-x no-scrollbar">
-        <li className="flex-none max-h-[515px] snap-center" tabIndex={9}>
+        <li
+          className="flex-none pointer-events-none max-h-[515px] snap-center"
+          tabIndex={9}
+        >
           <img
             className="h-full max-h-[515px] w-auto snap-center"
             src="/photos/IMG_3705.webp"
           />
         </li>
-        <li className="flex-none max-h-[515px] snap-center" tabIndex={10}>
+        <li
+          className="flex-none pointer-events-none max-h-[515px] snap-center"
+          tabIndex={10}
+        >
           <img
             className="h-full max-h-[515px] "
             src="/photos/IMG_trabajo.webp"
           />
         </li>
-        <li className="flex-none max-h-[515px] snap-center" tabIndex={11}>
+        <li
+          className="flex-none pointer-events-none max-h-[515px] snap-center"
+          tabIndex={11}
+        >
           <img
             className="h-full max-h-[515px] "
             src="/photos/IMG_tanque.webp"
           />
         </li>
-        <li className="flex-none max-h-[515px] snap-center" tabIndex={12}>
+        <li
+          className="flex-none pointer-events-none max-h-[515px] snap-center"
+          tabIndex={12}
+        >
           <img
             className="h-full max-h-[515px] w-auto"
             src="/photos/IMG_4173.webp"
@@ -39,12 +53,14 @@ export const FavoriteSection = () => {
           tabIndex={13}
         >
           <div className="bg-black h-full w-full">
-            <p
-              className="text-2xl text-white font-bold p-14 py-48 text-wrap"
-              tabIndex={14}
-            >
-              Te invito a ver las demás fotos ---&gt;{" "}
-            </p>
+            <Link href="/photos">
+              <p
+                className="text-2xl text-white font-bold p-14 py-48 text-wrap"
+                tabIndex={14}
+              >
+                Te invito a ver las demás fotos ---&gt;{" "}
+              </p>
+            </Link>
           </div>
         </li>
       </ul>
--- components/home/Middle.tsx
+++ components/home/Middle.tsx
@@ -1,6 +1,6 @@
 export const Middle = () => {
   return (
-    <div className="relative h-[400px] sm:h-[450px] md:h-[500px] overflow-hidden">
+    <div className="relative h-[400px] sm:h-[450px] md:h-[500px] overflow-hidden pointer-events-none">
       <div
         tabIndex={7}
         className="absolute left-4 top-6 sm:top-12 md:top-24 z-10 font-bold text-2xl sm:text-3xl max-w-[30ch] text-white tracking-wide pr-8 pl-2 text-balance flex flex-col gap-32 lg:text-right"
@@ -10,7 +10,7 @@ export const Middle = () => {
       </div>
       <div className="block relative h-[400px] sm:h-[450px] md:h-[500px] clip-scroll">
         <img
-          className="brightness-[.25] object-cover fixed top-0 left-0 w-full h-full"
+          className="brightness-[.25] object-cover fixed top-0 left-0 w-full h-full lg:w-2/3 lg:right-0 lg:left-auto lg:bottom-0"
           src="/photos/IMG_sendero.webp"
         />
       </div>
