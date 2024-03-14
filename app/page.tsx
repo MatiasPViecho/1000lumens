c439e9451753bb0cb69bce99d3239ad13a6443c5
Matias V <mpviecho@gmail.com>
2024-03-13 23:41:25 -0300

    feat: add photos and bottom section

--- app/page.tsx
+++ app/page.tsx
@@ -18,8 +18,14 @@ export default function Home() {
           </div>
       </div>
       <div className="py-4">
-      <h6 className="text-balance px-4 text-black font-bold text-3xl ">Te dejo mis favoritas!</h6>
+        <h6 className="text-balance px-4 text-black font-bold text-3xl ">Te dejo mis favoritas!</h6>
       </div>
+      <ul className="flex w-full gap-2 overflow-x-scroll px-4 py-2">
+        <li className="w-full h-auto flex-none"><img  src="/photos/IMG_3705.webp" /></li>
+        <li className="w-full h-auto flex-none"><img  src="/photos/IMG_trabajo.webp" /></li>
+        <li className="w-full h-auto flex-none"><img  src="/photos/IMG_tanque.webp" /></li>
+        <li className="w-full h-auto flex-none"><img  src="/photos/IMG_4173.webp" /></li>
+      </ul>
     </main>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
     return(
         <header className="fixed top-0 z-50 w-full ">
-            <ul className="flex gap-4 justify-center pt-2 pb-3  bg-black/50 backdrop-blur-sm max-w-72  rounded-b-full  text-white mx-auto">
+            <ul className="flex gap-4 justify-center pt-2 pb-3 shadow-lg bg-black/50 backdrop-blur-sm max-w-72  rounded-b-full  text-white mx-auto">
                 <li>Inicio</li>
                 <li>Mis Fotos</li>
                 <li>Sobre Mí</li>
