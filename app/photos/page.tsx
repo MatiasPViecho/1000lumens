207af8b5427c4b2288cffb1eb20fe5e88a93240a
Matias V <mpviecho@gmail.com>
2024-03-23 20:28:58 -0300

    feat: add my photos page

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -1,21 +1,58 @@
+import { FramedPhoto } from "@/components/FramedPhoto";
+
 export default function PhotosPage() {
   return (
-    <section className="flex flex-col gap-6 md:grid md:grid-cols-2 lg:grid-cols-3 lg:max-w-5xl lg:mx-auto lg:py-16 overflow-hidden">
+    <section className="flex flex-col gap-6 md:grid md:grid-cols-2 lg:grid-cols-3 lg:max-w-5xl lg:mx-auto py-16 px-4 lg:px-0 overflow-hidden">
       <div className="flex flex-col gap-6">
-        <div className="aspect-video bg-red-300"></div>
-        <div className="aspect-square bg-red-300"></div>
-        <div className="aspect-video bg-red-300"></div>
-        <div className="aspect-video bg-red-300"></div>
+        <FramedPhoto
+          src="/photos/my-photos-page/20240321-IMG_5709.webp"
+          alt="Sendero sinuodal por un serro en Jujuy"
+        />
+        <FramedPhoto
+          src="/photos/my-photos-page/20240321-IMG_5710.webp"
+          alt="Sendero sinuodal por un serro en Jujuy"
+        />
+
+        <FramedPhoto
+          src="/photos/my-photos-page/20240321-IMG_5714.webp"
+          alt="Sendero sinuodal por un serro en Jujuy"
+        />
       </div>
       <div className="flex flex-col gap-6">
-        <div className="aspect-video bg-red-300"></div>
-        <div className="aspect-[9/16] bg-red-300"></div>
-        <div className="aspect-video bg-red-300"></div>
+        <FramedPhoto
+          src="/photos/my-photos-page/20240321-IMG_5748-2.webp"
+          alt="Sendero sinuodal por un serro en Jujuy"
+        />
+        <FramedPhoto
+          horizontal
+          src="/photos/my-photos-page/20240321-IMG_5759.webp"
+          alt="Sendero sinuodal por un serro en Jujuy"
+        />
+        <FramedPhoto
+          src="/photos/my-photos-page/20240321-IMG_5756.webp"
+          alt="Sendero sinuodal por un serro en Jujuy"
+        />
       </div>
       <div className="flex flex-col gap-6">
-        <div className="aspect-video bg-red-300"></div>
-        <div className="aspect-video bg-red-300"></div>
-        <div className="aspect-[9/16] bg-red-300"></div>
+        <FramedPhoto
+          horizontal
+          src="/photos/my-photos-page/20190107-IMG_2696.webp"
+          alt="Sendero sinuodal por un serro en Jujuy"
+        />
+        <FramedPhoto
+          src="/photos/my-photos-page/20190107-IMG_2719-2.webp"
+          alt="Sendero sinuodal por un serro en Jujuy"
+        />
+
+        <FramedPhoto
+          src="/photos/my-photos-page/20240321-IMG_5797.webp"
+          alt="Sendero sinuodal por un serro en Jujuy"
+        />
+
+        <FramedPhoto
+          src="/photos/my-photos-page/20240321-IMG_5717.webp"
+          alt="Sendero sinuodal por un serro en Jujuy"
+        />
       </div>
     </section>
   );
