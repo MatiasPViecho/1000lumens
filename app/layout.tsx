d67ad0e51ca8b688453ae04b91b35e96b6e3875c
Matias V <mpviecho@gmail.com>
2024-03-24 16:13:59 -0300

    feat: add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,42 @@ import { Header } from "@/components/Header";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Matias Viecho | Fotografía",
+  title: {
+    template: "%s | 1000Lumens",
+    default: "1000Lumens",
+  },
   description: "Portfolio fotogáfico de Matías Pérez Viecho",
   icons: {
     icon: "/favicon.ico",
+  },
+  keywords: [
+    "Next.js",
+    "React",
+    "JavaScript",
+    "Photography",
+    "Fotografía NextJS",
+  ],
+  authors: [{ name: "Matias Perez Viecho" }],
+  creator: "Matias Perez Viecho",
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  openGraph: {
+    title: "Fotografías de Matías Pérez Viecho",
+    description:
+      "1000 lumens es un proyecto dedicado a mostrar de un modo rápido y sencillo las fotos que saqué a lo largo de mi carrera",
+    images: [
+      {
+        url: "/1000LUMENS.webp",
+        width: 800,
+        height: 600,
+        alt: "Portada 1000Lumens, showcase de mis fotografías",
+      },
+    ],
+    locale: "es_AR",
+    type: "website",
   },
 };
 
