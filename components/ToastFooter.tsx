e43d7e2079696a57977c6d26dfd4809aa8fab504
Matias V <mpviecho@gmail.com>
2024-03-28 15:19:43 -0300

    feat: add ToastFooter component

---
+++ components/ToastFooter.tsx
@@ -0,0 +1,5 @@
+export const ToastFooter = () => {
+  return (
+    <div className="sticky mx-auto bottom-8 w-max px-4 py-2 text-md font-semibold bg-white border-black border-1 rounded-full"></div>
+  );
+};
